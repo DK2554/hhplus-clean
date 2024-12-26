cae4020e3d9f7fd4017a80a316f98097479cd89f
DK2554 <ldu0215@gmail.com>
2024-12-26 13:17:15 +0900

    초기 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,41 @@
+-- User Table
+CREATE TABLE "user" (
+                      id INT PRIMARY KEY,
+                      user_name VARCHAR NOT NULL,
+                      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      updated_at TIMESTAMP
+);
+
+-- Lecture Table
+CREATE TABLE lecture (
+                         id INT PRIMARY KEY,
+                         lecture_name VARCHAR NOT NULL,
+                         max_capacity INT NOT NULL,
+                         current_capacity INT DEFAULT 0,
+                         application_start_at TIMESTAMP NOT NULL,
+                         application_end_at TIMESTAMP NOT NULL,
+                         lecture_start TIMESTAMP NOT NULL,
+                         lecture_end TIMESTAMP NOT NULL,
+                         create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP
+);
+
+-- Instructor Table
+CREATE TABLE instructor (
+                            id INT PRIMARY KEY,
+                            instructor_name VARCHAR NOT NULL,
+                            create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP
+);
+
+-- Lecture Enrollment Table
+CREATE TABLE lecture_enrollment (
+                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    user_id INT NOT NULL,
+                                    lecture_id INT NOT NULL,
+                                    instructor_id INT NOT NULL,
+                                    enrollment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    updated_at TIMESTAMP,
+                                    status VARCHAR
+);
